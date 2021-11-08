827867b4023914877c039e2499c203434a408d64
draco servicios - Ing Kimberly Garces <kimberly.garces@draco.com.co>
2021-11-08 10:13:06 -0500

    ajustes fetch - creación de funccion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
+const [characters, setcharacters] = useState([])
 
 const initialUrl = "https://rickandmortyapi.com/api/character";
 
 const fetchCharacters = (url) => {
   fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => console.log(data.results))
     .catch(error => console.log(error))
 
 }
