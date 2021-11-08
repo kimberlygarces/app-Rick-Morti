2c2b172725e2baa824d70b6758f876b0a6c7f962
draco servicios - Ing Kimberly Garces <kimberly.garces@draco.com.co>
2021-11-08 09:50:15 -0500

    Uso de fetch - obtección de resultados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 
+import React, {useEffect} from "react";
 import Navbar from "./components/Navbar";
+
 function App() {
-  return (
+
+const initialUrl = "https://rickandmortyapi.com/api/character";
+
+const fetchCharacters = (url) => {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+}
+
+
+
+
+    useEffect(()=> {
+      fetchCharacters(initialUrl)
+
+    }, [])
+
+    return (
      <Navbar brand="Rick and Morty App"/>
   );
 }
