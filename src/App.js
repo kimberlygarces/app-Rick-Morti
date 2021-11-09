680b6d5718ae8da6f45ba400b35f8215af0becff
draco servicios - Ing Kimberly Garces <kimberly.garces@draco.com.co>
2021-11-08 22:06:45 -0500

    creacion de eventos para funcionamiento de oaginación

--- src/App.js
+++ src/App.js
@@ -7,16 +7,30 @@ import Pagination from "./components/Paginations";
 
 function App() {
 const [characters, setcharacters] = useState([])
+const [info, setInfo] = useState({})
 
 const initialUrl = "https://rickandmortyapi.com/api/character";
 
 const fetchCharacters = (url) => {
   fetch(url)
     .then(response => response.json())
-    .then(data => setcharacters(data.results))
+    .then(data => {
+      setcharacters(data.results);
+      setInfo(data.info);
+    })
     .catch(error => console.log(error))
+  }
 
-}
+  // realiza una nueva llamada a la api para mostrar los previos
+  const onPrevios = () => {
+    fetchCharacters(info.prev)
+  }
+
+  // realiza una llamada a la api para mostrar los seguientes
+  const onNext = () => {
+    fetchCharacters(info.next)
+  }
+
     useEffect(()=> {
       fetchCharacters(initialUrl)
 
@@ -28,10 +42,17 @@ const fetchCharacters = (url) => {
      <Navbar brand="Rick and Morty App"/>
 
       <div className="container mt-5">
-          <Pagination/>
+          <Pagination 
+              prev={info.prev} 
+              next={info.next} 
+              onPrevios={onPrevios} 
+              onNext={onNext}/>
           <Characters characters={characters}/>
-          <Pagination/>
-
+          <Pagination 
+              prev={info.prev} 
+              next={info.next} 
+              onPrevios={onPrevios} 
+              onNext={onNext}/>
       </div>
        
      </div>
--- src/components/Paginations.js
+++ src/components/Paginations.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-const Pagination = () => {
+const Pagination = ({prev, next, onPrevios, onNext}) => {
+    
+    // funcion para el boton previous
+    const handlePrevious = () => {
+        onPrevios()
+    }
+    // funcion para el boton Next
+    const handleNext = () => {
+        onNext()
+
+    }
+
     return(
         <nav>
             <ul className="pagination justify-content-center">
+                {/* Los botones estan condicionados a que existan un NEXT o un Previos */}
+                {prev ? (
+                    <li className="page-item">
+                    <button className="page-link" onClick={handlePrevious}>Previus</button>
+                    </li>
+                ) : null }
+                {next ? (
+
                  <li className="page-item">
-                    <button className="page-link">Previus</button>
+                    <button className="page-link" onClick={handleNext}>Next</button>
                  </li>
-                 <li className="page-item">
-                    <button className="page-link">Next</button>
-                 </li>
+               ) : null }
+
             </ul>
         </nav>
     )
